b1578f81f8910bbf6f85b33b761844666ee64327
bruhunna <BRUHUNNA@volvocars.com>
2024-10-23 00:34:46 +0200

    Bug fix in calculation

--- netcore/Api.Tests/CongestionTaxControllerTests.cs
+++ netcore/Api.Tests/CongestionTaxControllerTests.cs
@@ -24,7 +24,7 @@ namespace Api.Tests
         {
             var mockDateStrings = new List<string> { "2013-02-08 06:27:00", "2013-02-08 06:20:27", "2013-02-08 14:35:00", "2013-02-08 15:29:00" };
             var request = new GetCongestionTaxRequest { VehicleType = "Emergency", TravelDates = mockDateStrings };
-            
+
             _mockCongestionTaxCalculatorService.Setup(service => service.GetTax(It.IsAny<Vehicle>(), It.IsAny<DateTime[]>())).ReturnsAsync(0);
 
 
@@ -33,8 +33,8 @@ namespace Api.Tests
 
             // Assert;
             Console.WriteLine($"Returned Data: {string.Join(", ", result)}");
-            
-            Assert.Equal(0, result);
+
+            Assert.Equal(result, 0);
         }
 
         [Fact]
@@ -50,9 +50,67 @@ namespace Api.Tests
             var result = await _controller.GetCongestionTaxFee(request);
 
             // Assert;
-            Console.WriteLine($"Returned Data: {string.Join(", ", result)}");
+            Assert.Equal(result, 13);
+        }
 
-            Assert.Equal(13, result);
+        [Fact]
+        public async Task GetCongestionTaxFee_InvalidVehicleType_ThrowsException()
+        {
+            var mockDateStrings = new List<string> { "2013-02-08 06:27:00", "2013-02-08 06:20:27", "2013-02-08 14:35:00", "2013-02-08 15:29:00" };
+            var request = new GetCongestionTaxRequest { VehicleType = "InvalidVehicleType", TravelDates = mockDateStrings };
+
+            _mockCongestionTaxCalculatorService.Setup(service => service.GetTax(It.IsAny<Vehicle>(), It.IsAny<DateTime[]>())).ReturnsAsync(13);
+
+            // Act
+            Func<Task> act = async () => await _controller.GetCongestionTaxFee(request);
+
+            // Assert
+            await Assert.ThrowsAsync<Exception>(act);
+        }
+
+        [Fact]
+        public async Task GetCongestionTaxFee_NullTravelDates_ThrowsException()
+        {
+            var request = new GetCongestionTaxRequest { VehicleType = "Car", TravelDates = null };
+
+            _mockCongestionTaxCalculatorService.Setup(service => service.GetTax(It.IsAny<Vehicle>(), It.IsAny<DateTime[]>())).ReturnsAsync(13);
+
+            // Act
+            Func<Task> act = async () => await _controller.GetCongestionTaxFee(request);
+
+            // Assert
+            await Assert.ThrowsAsync<Exception>(act);
+        }
+
+        [Fact]
+        public async Task GetCongestionTaxFee_TollFee_NotExceeded()
+        {
+            var request = new GetCongestionTaxRequest
+            {
+                VehicleType = "Car",
+                TravelDates = new List<string>
+                {
+                   "2013-02-08 06:27:00",
+                   "2013-02-08 06:20:27",
+                   "2013-02-08 14:35:00",
+                   "2013-02-08 15:29:00",
+                   "2013-02-08 15:47:00",
+                   "2013-02-08 16:01:00",
+                   "2013-02-08 16:48:00",
+                   "2013-02-08 17:49:00",
+                   "2013-02-08 18:29:00",
+                   "2013-02-08 18:35:00",
+                }
+            };
+
+            _mockCongestionTaxCalculatorService.Setup(service => service.GetTax(It.IsAny<Vehicle>(), It.IsAny<DateTime[]>())).ReturnsAsync(60);
+
+            // Act
+            var result = await _controller.GetCongestionTaxFee(request);
+
+            // Assert
+
+            Assert.Equal(result, 60);
         }
     }
 }
--- netcore/Api/Services/CongestionTaxCalculatorService.cs
+++ netcore/Api/Services/CongestionTaxCalculatorService.cs
@@ -20,11 +20,8 @@ public class CongestionTaxCalculatorService : ICongestionTaxCalculatorService
         {
             int nextFee = await GetTollFee(date, vehicle);
             int tempFee = await GetTollFee(intervalStart, vehicle);
-
-            long diffInMillies = date.Millisecond - intervalStart.Millisecond;
-            long minutes = diffInMillies / 1000 / 60;
-
-            if (minutes <= 60)
+            TimeSpan diffInMillies = date - intervalStart;
+            if (diffInMillies.TotalMinutes <= 60)
             {
                 if (totalFee > 0) totalFee -= tempFee;
                 if (nextFee >= tempFee) tempFee = nextFee;
@@ -63,12 +60,11 @@ public class CongestionTaxCalculatorService : ICongestionTaxCalculatorService
 
         int hour = date.Hour;
         int minute = date.Minute;
-
         if (hour == 6 && minute >= 0 && minute <= 29) return 8;
         else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
         else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
         else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-        else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
+        else if (hour >= 8 && minute >= 30 || hour <= 14 && minute <= 59) return 8;
         else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
         else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
         else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
